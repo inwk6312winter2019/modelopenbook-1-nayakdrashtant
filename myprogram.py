e8dda7c01f95ca0a5417d4365f90eed30b08b149
= <drahtantnayak@dal.ca>
2019-02-06 13:49:43 -0400

    Complete subtask 3

--- myprogram.py
+++ myprogram.py
@@ -33,7 +33,27 @@ def replace_str():
             newfile.write(line)
         else:
             newfile.write(my)
-#      newfile.close() 
+
+def create_list():
+    myfile = open("running-config.cfg","r")
+    transit_access_in = []
+    fw_management_access_in = []
+    global_access = []
+    for my in myfile:
+        if "transit_access_in" in my:
+            my = my.strip()
+            transit_access_in.append(my)
+        if "fw-management_access_in" in my:
+            my = my.strip()
+            fw_management_access_in.append(my)
+        if "global_access" in my:
+            my = my.strip()
+            global_access.append(my)
+
+    print("\ntransit_access list:",transit_access_in)
+    print("\nfw_management_list:",fw_management_access_in)
+    print("\nglobal_access list:",global_access)
 
 print(list_ifname_ip())
 replace_str()
+create_list()
