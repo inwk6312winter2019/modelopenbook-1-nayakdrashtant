c821ff01d669f4a721967d7b0c89a78aba91d2a5
= <drahtantnayak@dal.ca>
2019-02-06 12:18:32 -0400

    Complete subtask 1 from given task

--- myprogram.py
+++ myprogram.py
@@ -3,8 +3,19 @@ import string
 def list_ifname_ip():
     mydict = dict()
     myfile = open("running-config.cfg","r")
+   # mydict["nameif"] = {}
+    counter = 0
     for my in myfile:
-        print(my.strip())
-
+        mywhi = my.strip()
+        if "ip address" in mywhi:
+            if "no ip address" not in mywhi:
+                mylist = mywhi.split()
+                ip = mylist[2]
+                mask = mylist[3]
+                key = "nameif" + str(counter)
+                counter += 1
+                com = "(" + str(ip) + "," + str(mask) + ")"
+                mydict[key] = com 
+    print(mydict)
 
 list_ifname_ip()
